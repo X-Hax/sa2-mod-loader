b4f1a92accf095768f858dc62491c71fa2990161
MainMemory <mainmemory@yahoo.com>
2019-11-03 19:28:11 -0600

    Adding some kind of testspawn.

--- SA2ModLoader/dllmain.cpp
+++ SA2ModLoader/dllmain.cpp
@@ -19,6 +19,7 @@
 #include "CodeParser.hpp"
 #include "Events.h"
 #include "FadeFix.h"
+#include "testspawn.h"
 
 static std::thread* window_thread = nullptr;
 
@@ -1599,6 +1600,8 @@ void __cdecl InitMods(void)
 	WriteJump((void*)0x0077E897, OnInput);
 	WriteJump((void*)0x00441D41, OnControl);
 	WriteJump((void*)0x00441EEB, OnControl);
+
+	TestSpawnCheckArgs(helperFunctions);
 
 	if (MainUserConfig->data.Fullscreen == 0)
 	{
