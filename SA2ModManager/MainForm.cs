15a456a4454934d8e49e3bdf6eb81659e3199dc9
Sora <sora.x@hotmail.fr>
2024-04-03 00:32:03 +0200

    remove update

--- SA2ModManager/MainForm.cs
+++ SA2ModManager/MainForm.cs
@@ -611,57 +611,6 @@ namespace SA2ModManager
 			checkedForUpdates = true;
 			loaderini.UpdateTime = DateTime.UtcNow.ToFileTimeUtc();
 
-			if (!File.Exists("sa2mlver.txt"))
-			{
-				return false;
-			}
-
-			using (var wc = new WebClient())
-			{
-				try
-				{
-					string msg = wc.DownloadString("http://mm.reimuhakurei.net/toolchangelog.php?tool=sa2ml&rev=" + File.ReadAllText("sa2mlver.txt"));
-
-					if (msg.Length > 0)
-					{
-						using (var dlg = new UpdateMessageDialog("SA2", msg.Replace("\n", "\r\n")))
-						{
-							if (dlg.ShowDialog(this) == DialogResult.Yes)
-							{
-								DialogResult result = DialogResult.OK;
-								do
-								{
-									try
-									{
-										if (!Directory.Exists(updatePath))
-										{
-											Directory.CreateDirectory(updatePath);
-										}
-									}
-									catch (Exception ex)
-									{
-										result = MessageBox.Show(this, "Failed to create temporary update directory:\n" + ex.Message
-																	   + "\n\nWould you like to retry?", "Directory Creation Failed", MessageBoxButtons.RetryCancel);
-										if (result == DialogResult.Cancel) return false;
-									}
-								} while (result == DialogResult.Retry);
-
-								using (var dlg2 = new LoaderDownloadDialog("http://mm.reimuhakurei.net/sa2mods/SA2ModLoader.7z", updatePath))
-									if (dlg2.ShowDialog(this) == DialogResult.OK)
-									{
-										Close();
-										return true;
-									}
-							}
-						}
-					}
-				}
-				catch
-				{
-					MessageBox.Show(this, "Unable to retrieve update information.", "SA2 Mod Manager");
-				}
-			}
-
 			return false;
 		}
 
