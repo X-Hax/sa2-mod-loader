9aa5b75680dff2ef8b83a03f1f56359ef9acdbcc
ItsEasyActually <darkspines35@gmail.com>
2024-05-13 14:51:13 -0400

    Reverting previous changes, some window related behavior will be impacted until the next update.

--- SA2ModLoader/window.cpp
+++ SA2ModLoader/window.cpp
@@ -114,8 +114,8 @@ static void update_innerwindow(int w, int h)
 	}
 	else if (customWindowSize)
 	{
-		innerWidth = MainUserConfig->data.Width;
-		innerHeight = MainUserConfig->data.Height;
+		innerWidth = customWindowWidth;
+		innerHeight = customWindowHeight;
 	}
 	else
 	{
@@ -315,41 +315,38 @@ enum screenmodes { window_mode, borderless_mode, custom_mode, fullscreen_mode };
 void PatchWindow(const LoaderSettings& settings, std::wstring& borderimg)
 {
 	if (IS_FULLSCREEN)
+	{
 		return;
-
-	maintainAspectRatio = settings.KeepAspectWhenResizing;
-	windowResize = settings.ResizableWindow;
-	screenNum = settings.ScreenNum;
-	disableExitPrompt = settings.DisableExitPrompt;
-	pauseWhenInactive = settings.PauseWhenInactive;
+	}
 
 	switch (settings.ScreenMode)
 	{
 	case window_mode:
 		windowedFullscreen = false;
+		windowResize = settings.ResizableWindow;
 		customWindowSize = false;
 		break;
 	case borderless_mode:
 		windowedFullscreen = true;
+		windowResize = settings.ResizableWindow;
 		customWindowSize = false;
-
-		maintainAspectRatio = true;	// Required for the inner window to scale to the outer window, does not alter the render resolution.
 		break;
 	case custom_mode:
 		windowedFullscreen = false;
+		windowResize = false;
 		customWindowSize = true;
 		customWindowWidth = settings.WindowWidth;
 		customWindowHeight = settings.WindowHeight;
-
-		// Forced Settings
-		maintainAspectRatio = true;	// Required for the inner window to scale to the outer window, does not alter the render resolution.
-		windowResize = false;		// Must be false because we do not currently update the inner window with the outer window when a resize occurs.
 		break;
 	default:
 		return;
 	}
 
-	windowWrapper = maintainAspectRatio || windowedFullscreen || customWindowSize;
+	maintainAspectRatio = settings.MaintainAspectRatio;
+	windowWrapper = maintainAspectRatio || (windowedFullscreen && customWindowSize);
+	screenNum = settings.ScreenNum;
+	disableExitPrompt = settings.DisableExitPrompt;
+	pauseWhenInactive = settings.PauseWhenInactive;
 
 	// Replace the default window procedure
 	WriteJump(reinterpret_cast<void*>(0x00401810), WndProc_Hook);
